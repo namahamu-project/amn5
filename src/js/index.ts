1cf442652cdddb8794d7db358a3a3caf8d047579
polyxx <polyxx@users.noreply.github.com>
2019-04-19 19:01:25 +0900

    use webpack

---
+++ src/js/index.ts
@@ -0,0 +1,14 @@
+import MoveTo from 'moveto';
+import '../scss/index.scss';
+
+const trigger = document.getElementsByClassName('back-to-top')[0];
+const target = document.documentElement || document.body;
+const calcScrollTop = () => target.scrollTop || 0;
+
+document.addEventListener('scroll', () => {
+  const scrollTop = calcScrollTop();
+  trigger.setAttribute('data-is-visible', (scrollTop > 100).toString());
+});
+
+const moveTo = new MoveTo();
+moveTo.registerTrigger(trigger);
---
+++ webpack.dev.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const sass = require('sass');
+const fiber = require('fibers');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const src = path.join(__dirname, 'src');
+const dist = path.join(__dirname, 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(src, 'js/index'),
+  output: {
+    filename: 'app.js',
+    path: dist,
+    publicPath: '/'
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              fiber,
+              implementation: sass
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: dist,
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    port: 8000
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(src, '/html/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+    })
+  ]
+};
