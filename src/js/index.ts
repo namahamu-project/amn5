c9d1ed9db46a88b14a41788ab3f2c027e668fc59
polyxx <polyxx@users.noreply.github.com>
2019-04-21 01:07:15 +0900

    add animations

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,9 @@
+import AOS from 'aos';
 import 'lazysizes';
 import MoveTo from 'moveto';
 import '../scss/index.scss';
 
+/* back to top */
 const trigger = document.getElementsByClassName('back-to-top')[0];
 const target = document.documentElement || document.body;
 const calcScrollTop = () => target.scrollTop || 0;
@@ -11,5 +13,9 @@ document.addEventListener('scroll', () => {
   trigger.setAttribute('data-is-visible', (scrollTop > 100).toString());
 });
 
+/* moveto */
 const moveTo = new MoveTo();
 moveTo.registerTrigger(trigger);
+
+/* aos */
+AOS.init();
